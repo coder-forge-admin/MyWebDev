997931fe7e0298878227ae6fe3f551e7825055d7
johnmlhll <johnmlhll@yahoo.com>
2016-02-21 19:08:38 +0000

    File Pack To get Expressjs Server Running with Nodejs

---
+++ main.js
@@ -0,0 +1,11 @@
+//Function is a router between server.js and client for requests
+module.exports = function(app)
+{
+  app.get('/',function(req,res){
+    res.render('index.html');
+  });
+  app.get('/about',function(req,res){
+    res.render('about.html');
+  });
+}
+  
---
+++ server.js
@@ -0,0 +1,14 @@
+//Instantiating express and assigning this app for server.js
+var express = require('express');
+var app = express();
+
+//Calling main.js in router folder set a callback and rendering using ejs
+require('./router/main')(app);
+app.set('views',__dirname+'/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+//Instantiating the server listening from port 3000 for a callback
+var server = app.listen(3000, function(){
+  console.log("We have started on port 3000");
+});
